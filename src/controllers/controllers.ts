49deccf063f9eca87961a504a8e1e270c00f705a
saif-elhady <saifelhady2004@gmail.com>
2024-06-22 00:47:30 +0300

    feat: Add method handlers and controllers

---
+++ src/controllers/controllers.ts
@@ -0,0 +1,62 @@
+import { Model, Document } from "mongoose";
+import { Request, Response } from "express"; 
+
+const Note: Model<Document> = require('../models/model');
+
+//GET homePage
+const noteIndex = (req: Request, res: Response): void => {
+    Note.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render("notes/index", { title: 'All Notes', notes: result  });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+//GET detailsPage
+const noteDetails = (req: Request, res: Response): void => {
+    const id = req.params.id;
+    Note.findById(id)
+        .then((result) => {
+            res.render('notes/details',{ blog: result, title: 'Note Details' })
+        })
+}
+
+//GET createpage
+const noteCreate = (req:Request, res:Response):void => {
+    res.render('notes/create', { title: 'Create an new note' });
+}
+
+//POST createPage
+const noteCreatePost = (req: Request, res: Response):void => {
+    const note = new Note(req.body);
+    note.save()
+        .then(() => {
+            res.redirect('/notes');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+//DELETE note
+const noteDelete= (req:Request, res:Response):void => {
+    const id = req.params.id;
+
+    Note.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/notes'})
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+module.exports = {
+    noteIndex,
+    noteDetails,
+    noteCreate,
+    noteCreatePost,
+    noteDelete,
+}
